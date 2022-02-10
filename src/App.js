c7e2b123c1077a4d5702646cb392bc1410447196
Keerti Sharma <keerti_sharma@intuit.com>
2022-02-09 17:28:56 -0800

    Initial module and components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {VotingTool} from "./features/ballot/VotingTool";
+import {RegistrationTool} from "./features/voter/RegistrationTool";
+import {ElectionTool} from "./features/election/ElectionTool";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <VotingTool/>
+      <RegistrationTool/>
+      <ElectionTool/>
     </div>
   );
 }
---
+++ src/features/voter/RegistrationTool.js
@@ -0,0 +1,8 @@
+
+export const RegistrationTool = () => {
+    return (
+        <div>
+            RegistrationTool
+        </div>
+    );
+}
