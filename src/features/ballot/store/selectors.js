8242847ca2b012c76b398f63c1a56f0b291b521e
ibiwan-intuit <83093935+ibiwan-intuit@users.noreply.github.com>
2022-02-10 20:45:23 -0800

    fix selector bug

--- src/features/ballot/store/selectors.js
+++ src/features/ballot/store/selectors.js
@@ -1,12 +1,12 @@
 export const selectBallotsByElection = (electionId) => (state) => {
-    console.log("BallotSelector", state);
+    ;
 
     return state.ballotTool.ballots.filter ((ballot) => {
-        console.log("BallotSelector", ballot, electionId);
+        ;
 
         return ballot.electionId === electionId}
     );
 }
 
 // export const selectElectionViewMode = (state) => state.electionTool.mode;
-export const selectSelectedElectionId = (state) => state.electionTool.selectedElectionId;
+export const selectSelectedElectionId = (state) => state.ballotTool.selectedElectionId;
